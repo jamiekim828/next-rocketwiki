d165e37b8ca13ec03c04e28d2fe9f9cce7b96ada
jamiekim828 <jamiekim828@gmail.com>
2023-04-18 11:23:40 +0200

    render thumbnails and title

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import getWikiResult from '@/lib/getWikiResult';
+import Items from './components/Items';
 
 type Props = {
   params: {
     searchTerm: string;
   };
 };
+
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResult(searchTerm);
+  const data = await wikiData;
+  const displayTerm = searchTerm.replaceAll('%20', ' ');
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`,
+    };
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search results for ${displayTerm}`,
+  };
+}
 
 export default async function SearchResults({ params: { searchTerm } }: Props) {
   const wikiData: Promise<SearchResult> = getWikiResult(searchTerm);
@@ -15,8 +33,8 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
   const content = (
     <main className='bg-slate-200 mx-auto max-w-lg py-1 min-h-screen'>
       {results ? (
-        Object.values(results).map((res, i) => {
-          return <p key={i}>{JSON.stringify(res)}</p>;
+        Object.values(results).map((res) => {
+          return <Items key={res.pageid} result={res} />;
         })
       ) : (
         <h2 className='p-2 text-xl'>{`${searchTerm} Not Found`}</h2>
